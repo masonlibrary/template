5a7b7b8a01c435834b27341e62dea8dbe428720b
Webster Massingham <wmassingham@gmail.com>
2014-04-14 15:28:33 -0400

    add variable to store connection

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	mysqli_connect('mysql.example.com', 'username', 'password', 'database_name')
-	or die('Failed to connect to the database');
+	$dbc = mysqli_connect('mysql.example.com', 'username', 'password', 'database_name')
+		or die('Failed to connect to the database');
 
 ?>
