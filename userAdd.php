bdbcfffe0f04af75c1571e6892c42984ce9c85fb
Webster Massingham <wmassingham@gmail.com>
2014-04-25 15:38:03 -0400

    rewrite userAdd.php for template project

--- userAdd.php
+++ userAdd.php
@@ -1,99 +1,82 @@
 <?php
-	require_once('control/connection.php');
-	require_once('control/startSession.php');
 
-	if (!isset($_SESSION['userID'])) { header("Location: login.php"); exit("Not logged in"); }
-	if ($_SESSION['roleID'] != 1) { header("HTTP/1.0 403 Forbidden"); die("Unauthorized"); } // Only let admins add users
-	
-	$page_title = 'Add user';
-	include('includes/header.php');
+	require_once 'includes/session.php';
+	require_once 'includes/connection.php';
+	require_once 'includes/password.php';
+
+	if (!$site_uses_auth) { header('Location: ' . $site_base_url); }
+	if (!isset($_SESSION['userID'])) { header('Location: ' . $site_base_url); exit("Not logged in"); }
+	if ($_SESSION['userClass'] > 0) { header("HTTP/1.0 403 Forbidden"); die("Unauthorized"); } // Only let admins add users
 	
 	if($_POST) {
 		try {
 			mysqli_autocommit($dbc, false);
 
 			if(!$_POST['newpw1']) throw new Exception("Couldn't add user: Blank password");
-			if($_POST['newpw1'] != $_POST['newpw2']) throw new Exception("Couldn't add user: Passwords don't match");
-			$stmt = mysqli_prepare($dbc, 'insert into users (userName, userPass) values (?, ?)');
-			$newpwhash = sha1($_POST['newpw1']);
-			mysqli_stmt_bind_param($stmt, 'ss', $_POST['username'], $newpwhash);
-			if(!mysqli_stmt_execute($stmt)) throw new Exception("Couldn't add user: " . mysqli_error($dbc));
+			if($_POST['newpw1'] != $_POST['newpw2']) throw new Exception('Failed to add user: Password mismatch');
+
+			$stmt = mysqli_prepare($dbc, 'insert into users (userName, userPass, userEmail, userClass) values (?, ?, ?, ?)');
+			$newpwhash = password_hash($_POST['newpw1'], PASSWORD_DEFAULT);
+			mysqli_stmt_bind_param($stmt, 'sssi', $_POST['username'], $newpwhash, $_POST['email'], $_POST['class']);
+			if(!mysqli_stmt_execute($stmt)) throw new Exception('Failed to add user: ' . mysqli_error($dbc));
 			
-			// Grab out the new user ID
-			$result = mysqli_query($dbc, 'select LAST_INSERT_ID() as userID');
-			if(!$result) throw new Exception("Couldn't select new user ID: " . mysqli_error($dbc));
-			$row = mysqli_fetch_assoc($result);
-			if(!$row) throw new Exception("Couldn't fetch new user ID: " . mysqli_error($dbc));
-			$userID = $row['userID'];
-
-			$stmt = mysqli_prepare($dbc, 'insert into people (ppleFName, ppleLName, ppleEmail) values (?, ?, ?)');
-			mysqli_stmt_bind_param($stmt, 'sss', $_POST['fname'], $_POST['lname'], $_POST['email']);
-			if(!mysqli_stmt_execute($stmt)) throw new Exception("Couldn't add account information: " . mysqli_error($dbc));
-			
-			// Grab out the new people ID
-			$result = mysqli_query($dbc, 'select LAST_INSERT_ID() as ppleID');
-			if(!$result) throw new Exception("Couldn't select new people ID: " . mysqli_error($dbc));
-			$row = mysqli_fetch_assoc($result);
-			if(!$row) throw new Exception("Couldn't fetch new people ID: " . mysqli_error($dbc));
-			$ppleID = $row['ppleID'];
-
-			$stmt = mysqli_prepare($dbc, 'insert into userroles (roleuserID, roleroleID) values (?, ?)');
-			mysqli_stmt_bind_param($stmt, 'ii', $userID, $_POST['role']);
-			if(!mysqli_stmt_execute($stmt)) throw new Exception("Couldn't add user role: " . mysqli_error($dbc));
-
-			// Only handle valid values for librarianmap
-			if(!($_POST['status']=='active' || $_POST['status']=='inactive')) throw new Exception("Couldn't add librarian map: Invalid status");
-			
-			$stmt = mysqli_prepare($dbc, 'insert into librarianmap (libmuserID, libmppleID, libmStatus) values (?, ?, ?)');
-			mysqli_stmt_bind_param($stmt, 'iis', $userID, $ppleID, $_POST['status']);
-			if(!mysqli_stmt_execute($stmt)) throw new Exception("Couldn't update user status: " . mysqli_error($dbc));
-
 			mysqli_commit($dbc);
 			
-			echo "User " . $_POST['username'] . 'added successfully.';
+			$_SESSION['dialogText'] = 'User ' . $_POST['username'] . ' added successfully.';
 			
 			mysqli_autocommit($dbc, true);
 		} catch (Exception $e) {
 			mysqli_rollback($dbc);
 			mysqli_autocommit($dbc, true);
-			echo "Error: " . $e->getMessage();
+			$_SESSION['dialogText'] = 'Error: ' . $e->getMessage();
 		}
-
 	}
-	
-	echo '<h1>Add user</h1>
-		<form method="post">
-		<table>
-			<tr><th>Account information</th><td></td></tr>
-			<tr><th><label for="username">Username</label></th><td><input id="username" name="username" type="text"/></td></tr>
-			<tr><th><label for="fname">First name</label></th><td><input id="fname" name="fname" type="text"/></td></tr>
-			<tr><th><label for="lname">Last name</label></th><td><input id="lname" name="lname" type="text"/></td></tr>
-			<tr><th><label for="email">Email</label></th><td><input id="email" name="email" type="email"/></td></tr>
-			<tr><th><label for="role">Role</label></th><td><select id="role" name="role">';
 
-			$s = array();
-			$stmt = mysqli_prepare($dbc, 'select roleID, roleName from roles');
-			mysqli_stmt_execute($stmt) or die("Failed to retrieve roles: " . mysqli_error($dbc));
-			mysqli_stmt_store_result($stmt);
-			mysqli_stmt_bind_result($stmt, $s['roleID'], $s['roleName']);
-			while (mysqli_stmt_fetch($stmt)) {
-				echo '<option value="'.$s['roleID'].'">'.$s['roleName'].'</option>';
-			}
-			mysqli_stmt_free_result($stmt);
+	$page_title = 'Add user';
+	require_once 'includes/header.php';
 
-			echo '</select></td></tr>
-			<tr><th><label for="status">Status</label></th><td>
-				<select id="status" name="status">
-					<option value="active">Active</option>
-					<option value="inactive">Inactive</option>
-				</select>
-			</td></tr>
-			<tr><th>Password</th><td></td></tr>
-			<tr><th><label for="newpw1">New password</label></th><td><input id="newpw1" name="newpw1" type="password"/></td></tr>
-			<tr><th><label for="newpw2">Confirm password</label></th><td><input id="newpw2" name="newpw2" type="password"/></td></tr>
-			<tr><th><input id="submit" name="submit" type="submit"/></th><td></td></tr>
-		</table>
-	</form>';
-	
-	include("includes/footer.php");
+?>
+
+<h1>Add user</h1>
+	<form method="post">
+	<table>
+		<tr>
+			<th>Account information</th>
+			<td></td>
+		</tr>
+		<tr>
+			<th><label for="username">Username</label></th>
+			<td><input id="username" name="username" type="text" maxlength="16"/></td>
+		</tr>
+		<tr>
+			<th><label for="email">Email</label></th>
+			<td><input id="email" name="email" type="email" maxlength="255"/></td>
+		</tr>
+		<tr>
+			<th><label for="class">Class</label></th>
+			<!-- Currently only Chrome and Opera support type="number", so for now we have to use jQuery UI. -->
+			<td><input id="class" name="class" type="text" value="0" min="0" title="The lower the number, the more permissions. 0 = All permissions."/></td>
+		</tr>
+		<tr>
+			<th>Password</th>
+			<td></td>
+		</tr>
+		<tr>
+			<th><label for="newpw1">New password</label></th>
+			<td><input id="newpw1" name="newpw1" type="password"/></td>
+		</tr>
+		<tr>
+			<th><label for="newpw2">Confirm password</label></th>
+			<td><input id="newpw2" name="newpw2" type="password"/></td>
+		</tr>
+		<tr>
+			<th><input id="submit" name="submit" type="submit"/></th>
+			<td></td>
+		</tr>
+	</table>
+</form>
+
+<?php
+	$jsOutput .= '$("#class").spinner();';
+	require_once 'includes/footer.php';
 ?>
