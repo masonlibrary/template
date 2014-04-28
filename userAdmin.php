4852372f6d3426cc1a982725acdaabf0e370241b
Webster Massingham <wmassingham@gmail.com>
2014-04-28 14:52:56 -0400

    rewrite userEdit.php and userAdmin.php for template project

--- userAdmin.php
+++ userAdmin.php
@@ -1,106 +1,55 @@
 <?php
-	require_once('control/connection.php');
-    require_once('control/startSession.php');
 
-	if (!isset($_SESSION['userID'])) { header("Location: login.php"); exit("Not logged in"); }
-	if($_SESSION['roleID'] != 1) { header("HTTP/1.0 403 Forbidden"); die("Unauthorized"); }
+	require_once 'includes/session.php';
+	require_once 'includes/connection.php';
+
+	if (!$site_uses_auth) { header('Location: ' . $site_base_url); }
+	if (!isset($_SESSION['userID'])) { header('Location: ' . $site_base_url); exit('Not logged in'); }
+	if ($_SESSION['userClass'] > 0) { header('HTTP/1.0 403 Forbidden'); die('Unauthorized'); } // Only let admins add users
 
 	$page_title = 'User administration';
-	include('includes/header.php');
+	require_once 'includes/header.php';
 
-	if (isset($_POST) && isset($_POST['userIDs'])) {
-		if (isset($_POST['bulkrolesubmit'])) {
+	if (isset($_POST) && isset($_POST['userIDs']) && isset($_POST['bulkclasssubmit'])) {
 
-			switch ($_POST['bulkrole']) {
-				case 'admin':
-					$role = 1;
-					break;
-				case 'power':
-					$role = 2;
-					break;
-				case 'user':
-					$role = 3;
-					break;
-				default:
-					break;
-			}
-
-			$stmt = mysqli_prepare($dbc, "update userroles set roleroleID=? where roleuserID=?");
-			foreach ($_POST['userIDs'] as $uid) {
-				mysqli_stmt_bind_param($stmt, "ii", $role, $uid);
-				mysqli_stmt_execute($stmt) or die("Failed to assign role $role to uid $uid:" . mysqli_error($dbc));
-			}
-
-		} else if (isset($_POST['bulkstatussubmit'])) {
-
-			switch ($_POST['bulkstatus']) {
-				// Not strictly necessary, but this way we're sure to not use user input in the query. -Webster
-				case 'active':
-					$status = 'active';
-					break;
-				case 'inactive':
-					$status = 'inactive';
-					break;
-				default:
-					break;
-			}
-
-			$stmt = mysqli_prepare($dbc, "update librarianmap set libmStatus=? where libmuserID=?");
-			foreach ($_POST['userIDs'] as $uid) {
-				mysqli_stmt_bind_param($stmt, "si", $status, $uid);
-				mysqli_stmt_execute($stmt) or die("Failed to assign status $status to uid $uid:" . mysqli_error($dbc));
-			}
-
+		$stmt = mysqli_prepare($dbc, 'update users set userClass=? where userID=?');
+		foreach ($_POST['userIDs'] as $uid) {
+			mysqli_stmt_bind_param($stmt, "ii", $_POST['class'], $uid);
+			mysqli_stmt_execute($stmt) or die('Failed to assign class '.$_POST['class'].' to uid '.$uid.': '. mysqli_error($dbc));
 		}
+
 	}
 
-	$query = 'select userID, userName, roleName, libmStatus, ppleLName, ppleFName, ppleEmail from users u
-		left outer join userroles ur on u.userID = ur.roleuserID
-		left outer join roles r on ur.roleroleID = r.roleID
-		left outer join librarianmap l on u.userID = l.libmuserID
-		left outer join people p on l.libmppleID = p.ppleID';
+	$query = 'select userID, userName, userEmail, userClass from users u';
 	$result = mysqli_query($dbc, $query) or die('Error querying for users: ' . mysqli_error($dbc));
 
 	?>
 	<a href="userAdd.php">Add new user</a>
 	<form method="post">
-		<select id="bulkrole" name="bulkrole">
-			<option value="" selected="selected">Change roles</option>
-			<option value="admin">Admin</option>
-			<option value="power">Power</option>
-			<option value="user">User</option>
-		</select>
-		<input type="submit" id="bulkrolesubmit" name="bulkrolesubmit" value="Apply">
-		<select id="bulkstatus" name="bulkstatus">
-			<option value="" selected="selected">Change status</option>
-			<option value="active">Active</option>
-			<option value="inactive">Inactive</option>
-		</select>
-		<input type="submit" id="bulkstatussubmit" name="bulkstatussubmit" value="Apply">
+		<label for="class">Bulk class change</label>
+		<input id="class" name="class" type="text" value="0" min="0" title="The lower the number, the more permissions. 0 = All permissions."/>
+		<input type="submit" id="bulkclasssubmit" name="bulkclasssubmit" value="Apply">
 		<table id="userlist"><thead><tr>
-		<th></th>
-		<th>User name</th>
-		<th>Role</th>
-		<th>Status</th>
-		<th>Last name</th>
-		<th>First name</th>
-		<th>Email</th>
-		<th></th>
+			<th></th>
+			<th>User name</th>
+			<th>User class</th>
+			<th>Email</th>
+			<th></th>
 		</tr></thead><tbody>
 	<?php
 	while ($row = mysqli_fetch_assoc($result)) {
-		echo '<tr><td><input type="checkbox" name="userIDs[]" id="userID'.$row['userID'].'" value="'.$row['userID'].'"/></td><td>'.
-			$row['userName'].'</td><td>'.
-			$row['roleName'].'</td><td>'.
-			$row['libmStatus'].'</td><td>'.
-			$row['ppleLName'].'</td><td>'.
-			$row['ppleFName'].'</td><td>'.
-			'<a href="mailto:'.$row['ppleEmail'].'">'.$row['ppleEmail'].'</a></td><td>'.
-			'<a href="userEdit.php?userID='.$row['userID'].'">edit</a></td></tr>';
+		echo '<tr>
+				<td><input type="checkbox" name="userIDs[]" id="userID'.$row['userID'].'" value="'.$row['userID'].'"/></td>
+				<td>'.$row['userName'].'</td>
+				<td>'.$row['userClass'].'</td>
+				<td><a href="mailto:'.$row['userEmail'].'">'.$row['userEmail'].'</a></td>
+				<td><a href="userEdit.php?userID='.$row['userID'].'">edit</a></td>
+			</tr>';
 	}
 	echo '</tbody></table></form>';
 
-	echo '<script type="text/javascript">$(document).ready( function(){$("#userlist").dataTable();} );</script>';
+	$jsOutput .= '$("#class").spinner();';
+	$jsOutput .= '$(document).ready( function(){$("#userlist").dataTable();} );';
 
 	include("includes/footer.php");
 ?>
--- userEdit.php
+++ userEdit.php
@@ -1,80 +1,76 @@
 <?php
-	require_once('control/connection.php');
-    require_once('control/startSession.php');
 
-	if (!isset($_SESSION['userID'])) { header("Location: login.php"); exit("Not logged in"); }
-	if (!isset($_GET['userID'])) { $_GET['userID'] = $_SESSION['userID']; } // No user means we load the logged-in user
-	if (!($_SESSION['roleID'] == 1 || $_GET['userID']) == $_SESSION['userID']) { header("HTTP/1.0 403 Forbidden"); die("Unauthorized"); }
+	require_once 'includes/session.php';
+	require_once 'includes/connection.php';
+	require_once 'includes/password.php';
+
+	if (!$site_uses_auth) { header('Location: ' . $site_base_url); }
+	if (!isset($_SESSION['userID'])) { header('Location: ' . $site_base_url); exit("Not logged in"); }
+
+	// No user means we load the logged-in user
+	if (isset($_GET['userID'])) {
+		$uid = $_GET['userID'];
+	} else {
+		$uid = $_SESSION['userID'];
+	}
 
 	// If we're not an admin, force the user we're editing to the user we're logged in as
-	$uid = ($_SESSION['roleID'] == 1 ? $_GET['userID'] : $_SESSION['userID']);
-
+	if ($_SESSION['userClass'] > 0) {
+		$uid = $_SESSION['userID'];
+	}
+	
 	$row = array();
-	$userDataStmt = mysqli_prepare($dbc, 'select userID, userName, userPass, roleroleID, libmStatus, ppleID, ppleFName, ppleLName, ppleEmail from users u
-		left outer join userroles ur on u.userID = ur.roleuserID
-		left outer join roles r on ur.roleroleID = r.roleID
-		left outer join librarianmap l on u.userID = l.libmuserID
-		left outer join people p on l.libmppleID = p.ppleID
-		where u.userid = ?');
-	mysqli_stmt_bind_param($userDataStmt, "i", $uid);
-	mysqli_stmt_execute($userDataStmt) or die("Failed to get user data: " . mysqli_error($dbc));
+	$userDataStmt = mysqli_prepare($dbc, 'select userID, userName, userPass, userEmail, userClass from users where userID = ?');
+	mysqli_stmt_bind_param($userDataStmt, 'i', $uid);
+	mysqli_stmt_execute($userDataStmt) or die('Failed to get user data: ' . mysqli_error($dbc));
 	mysqli_stmt_store_result($userDataStmt);
-	mysqli_stmt_bind_result($userDataStmt, $row['userID'], $row['userName'], $row['userPass'], $row['roleroleID'],
-		$row['libmStatus'], $row['ppleID'], $row['ppleFName'], $row['ppleLName'], $row['ppleEmail']);
+	mysqli_stmt_bind_result($userDataStmt, $row['userID'], $row['userName'], $row['userPass'], $row['userEmail'], $row['userClass']);
 	mysqli_stmt_fetch($userDataStmt);
 	mysqli_stmt_free_result($userDataStmt);
-//	$ppleID = $row['ppleID']; // used for setting real name below
 
 	if($_POST) {
 		try {
 			mysqli_autocommit($dbc, false);
 
 			if($_POST['newpw1'] && $_POST['newpw2']) {
-				if($uid != $_SESSION['userID'] && $_SESSION['roleID'] != 1) throw new Exception("Tried to change another user's password");
-				if($_SESSION['roleID'] != 1 && $row['userPass'] != sha1($_POST['oldpw'])) throw new Exception("Couldn't change password: Old password is invalid");
-				if($_POST['newpw1'] != $_POST['newpw2']) throw new Exception("Couldn't change password: New passwords don't match");
+				if($_SESSION['userClass'] > 0 && $uid != $_SESSION['userID']) throw new Exception('Tried to change another user\'s password');
+				if($_SESSION['userClass'] > 0 && !password_verify($_POST['oldpw'], $row['userPass'])) throw new Exception('Failed to change password: Old password is invalid');
+				if($_POST['newpw1'] != $_POST['newpw2']) throw new Exception('Failed to change password: New password mismatch');
 
 				$stmt = mysqli_prepare($dbc, 'update users set userPass=? where userID=?');
-				$newpwhash = sha1($_POST['newpw1']);
+				$newpwhash = password_hash($_POST['newpw1'], PASSWORD_DEFAULT);
 				mysqli_stmt_bind_param($stmt, 'si', $newpwhash, $uid);
-				if(!mysqli_stmt_execute($stmt)) throw new Exception("Couldn't update password: " . mysqli_error($dbc));
+				if(!mysqli_stmt_execute($stmt)) throw new Exception('Failed to update password: ' . mysqli_error($dbc));
 			}
 
-			$stmt = mysqli_prepare($dbc, 'update people set ppleFName=?, ppleLName=?, ppleEmail=? where ppleID=?');
-			mysqli_stmt_bind_param($stmt, 'sssi', $_POST['fname'], $_POST['lname'], $_POST['email'], $row['ppleID']);
-			if(!mysqli_stmt_execute($stmt)) throw new Exception("Couldn't update account information: " . mysqli_error($dbc));
-
-			// Only let admins change these values
-			if($_SESSION['roleID'] == 1) {
-				$stmt = mysqli_prepare($dbc, 'update userroles set roleroleID=? where roleuserID=?');
-				mysqli_stmt_bind_param($stmt, 'ii', $_POST['role'], $uid);
-				if(!mysqli_stmt_execute($stmt)) throw new Exception("Couldn't update user role: " . mysqli_error($dbc));
-
-				// Only handle valid values
-				if($_POST['status']=='active' || $_POST['status']=='inactive') {
-					$stmt = mysqli_prepare($dbc, 'update librarianmap set libmStatus=? where libmuserID=?');
-					mysqli_stmt_bind_param($stmt, 'si', $_POST['status'], $uid);
-					if(!mysqli_stmt_execute($stmt)) throw new Exception("Couldn't update user status: " . mysqli_error($dbc));
-				}
+			// Only let admins change some values
+			if($_SESSION['userClass'] == 0) {
+				$stmt = mysqli_prepare($dbc, 'update users set userEmail=?, userClass=? where userID=?');
+				mysqli_stmt_bind_param($stmt, 'sii', $_POST['email'], $_POST['class'], $uid);
+				if(!mysqli_stmt_execute($stmt)) throw new Exception('Failed to update account information: ' . mysqli_error($dbc));
+			} else {
+				$stmt = mysqli_prepare($dbc, 'update users set userEmail=? where userID=?');
+				mysqli_stmt_bind_param($stmt, 'si', $_POST['email'], $uid);
+				if(!mysqli_stmt_execute($stmt)) throw new Exception('Failed to update account information: ' . mysqli_error($dbc));
 			}
 
 			mysqli_commit($dbc);
 			mysqli_autocommit($dbc, true);
+			$_SESSION['dialogText'] = 'Successfully updated user data.';
 		} catch (Exception $e) {
 			mysqli_rollback($dbc);
 			mysqli_autocommit($dbc, true);
-			echo "Error: " . $e->getMessage();
+			$_SESSION['dialogText'] = 'Error: ' . $e->getMessage();
 		}
 	}
 
 	$page_title = $row['userName'] . ' - User edit';
-	include('includes/header.php');
+	require_once 'includes/header.php';
 
 	// reuse $userDataStmt from above
-	mysqli_stmt_execute($userDataStmt) or die("Failed to get user data: " . mysqli_error($dbc));
+	mysqli_stmt_execute($userDataStmt) or die('Failed to get user data: ' . mysqli_error($dbc));
 	mysqli_stmt_store_result($userDataStmt);
-	mysqli_stmt_bind_result($userDataStmt, $row['userID'], $row['userName'], $row['userPass'], $row['roleroleID'],
-		$row['libmStatus'], $row['ppleID'], $row['ppleFName'], $row['ppleLName'], $row['ppleEmail']);
+	mysqli_stmt_bind_result($userDataStmt, $row['userID'], $row['userName'], $row['userPass'], $row['userEmail'], $row['userClass']);
 	mysqli_stmt_fetch($userDataStmt);
 	mysqli_stmt_free_result($userDataStmt);
 
@@ -83,38 +79,22 @@
 		<table>
 			<tr><th>Account information</th><td></td></tr>
 			<tr><th><label for="username">Username</label></th><td><input id="username" name="username" type="text" disabled="disabled" value="'.$row['userName'].'"/></td></tr>
-			<tr><th><label for="fname">First name</label></th><td><input id="fname" name="fname" type="text" value="'.$row['ppleFName'].'"/></td></tr>
-			<tr><th><label for="lname">Last name</label></th><td><input id="lname" name="lname" type="text" value="'.$row['ppleLName'].'"/></td></tr>
-			<tr><th><label for="email">Email</label></th><td><input id="email" name="email" type="email" value="'.$row['ppleEmail'].'"/></td></tr>';
-			if ($_SESSION['roleID'] == 1) { // Only admins are allowed to see these fields
-				echo '<tr><th><label for="role">Role</label></th><td><select id="role" name="role">';
-
-				$s = array();
-				$stmt = mysqli_prepare($dbc, 'select roleID, roleName from roles');
-				mysqli_stmt_execute($stmt) or die("Failed to retrieve roles: " . mysqli_error($dbc));
-				mysqli_stmt_store_result($stmt);
-				mysqli_stmt_bind_result($stmt, $s['roleID'], $s['roleName']);
-				while (mysqli_stmt_fetch($stmt)) {
-					echo '<option value="'.$s['roleID'].'" '.($s['roleID'] == $row['roleroleID'] ? 'selected="selected"' : '').'>'.$s['roleName'].'</option>';
-				}
-				mysqli_stmt_free_result($stmt);
-
-				echo '</select></td></tr>
-				<tr><th><label for="status">Status</label></th><td>
-					<select id="status" name="status">
-						<option value="active"'.($row['libmStatus'] == 'active' ? ' selected="selected"' : '' ).'>Active</option>
-						<option value="inactive"'.($row['libmStatus'] == 'inactive' ? ' selected="selected"' : '' ).'>Inactive</option>
-					</select>
-				</td></tr>';
+			<tr><th><label for="email">Email</label></th><td><input id="email" name="email" type="email" value="'.$row['userEmail'].'"/></td></tr>';
+			// Only admins are allowed to see these fields
+			if ($_SESSION['userClass'] == 0) {
+				echo '<tr><th><label for="class">Class</label></th><td><input id="class" name="class" type="text" value="'.$row['userClass'].'" min="0" title="The lower the number, the more permissions. 0 = All permissions."/></td>';
 			}
 			echo '<tr><th>Password</th><td>(leave blank to keep current password)</td></tr>';
 			// Admins don't need to know the user's old password to change it
-			if($_SESSION['roleID'] != 1) echo '<tr><th><label for="oldpw">Old password</label></th><td><input id="oldpw" name="oldpw" type="password"/></td></tr>';
+			if($_SESSION['userClass'] > 0) {
+				echo '<tr><th><label for="oldpw">Old password</label></th><td><input id="oldpw" name="oldpw" type="password"/></td></tr>';
+			}
 			echo '<tr><th><label for="newpw1">New password</label></th><td><input id="newpw1" name="newpw1" type="password"/></td></tr>
 			<tr><th><label for="newpw2">Confirm password</label></th><td><input id="newpw2" name="newpw2" type="password"/></td></tr>
 			<tr><th><input id="submit" name="submit" type="submit"/></th><td></td></tr>
 		</table>
 	</form>';
 
+	$jsOutput .= '$("#class").spinner();';
 	include("includes/footer.php");
 ?>
